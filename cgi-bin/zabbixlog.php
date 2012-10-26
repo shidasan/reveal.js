44e8b832d0f8572801620cc79dd916364b455cf8
uchida <tsunade@miniz.localdomain>
2012-10-26 14:26:15 +0900

    fix bug

--- cgi-bin/zabbixlog.php
+++ cgi-bin/zabbixlog.php
@@ -5,7 +5,7 @@ $zabbix = new Zabbix($_ip);
 assert($zabbix->login($_user,$_pass));
 assert($zabbix->getItem("et2",array("name" => "Syslog")));
 $result = $zabbix->getHistory("dscript");
-echo json_encode(array("Method" => "ZabbixSyslog", "uuid" => com_create_guid(),
+echo json_encode(array("Method" => "ZabbixSyslog",
 			"Value" => array_map(function($value){ return json_decode(substr($value["value"],strpos($value["value"],"{")),true);}, $result["result"])));
 
 ?>
