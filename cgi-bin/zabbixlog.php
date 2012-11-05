b7e1174c811e222efd07646bb7b903191c30b4b0
uchida <tsunade@miniz.localdomain>
2012-11-05 15:23:48 +0900

    merged zabbixgraph

--- cgi-bin/zabbixlog.php
+++ cgi-bin/zabbixlog.php
@@ -3,9 +3,9 @@ require_once("action.php");
 require_once("conf.php");
 $zabbix = new Zabbix($_ip);
 assert($zabbix->login($_user,$_pass));
-//assert($zabbix->getItem("Zabbix server",array("name" => "Syslog")));
-assert($zabbix->getItem("et2",array("name" => "Syslog")));
-$result = $zabbix->getHistory("dscript");
+assert($zabbix->getItem("Zabbix server",array("name" => "Syslog")));
+//assert($zabbix->getItem("et2",array("name" => "Syslog")));
+$result = $zabbix->getHistory("minikonoha");
 echo json_encode(array("Method" => "ZabbixSyslog",
 			"Value" => array_map(function($value){ return json_decode(substr($value["value"],strpos($value["value"],"{")),true);}, $result["result"])));
 
--- js/myjs/graph.js
+++ js/myjs/graph.js
@@ -8,15 +8,15 @@ function Graph_init(){
 }
 
 function Cpu_get(){
-    Graph_get("#cpu",{"name":"util"});
+    Graph_get("#cpu",{"name":"util", "Server": $("#cpu_select option:selected").val()});
 }
 
 function Memory_get(){
-    Graph_get("#memory",{"name":"memory"});
+    Graph_get("#memory",{"name":"memory", "Server": $("#memory_select option:selected").val()});
 }
 
 function Network_get(){
-    Graph_get("#network",{"name":"eth0"});
+    Graph_get("#network",{"name":"eth0", "Server": $("#network_select option:selected").val()});
 }
 
 function Graph_get(selector,data) {
@@ -31,3 +31,7 @@ function Graph_get(selector,data) {
     dataType:'json'
   });
 }
+
+$("#cpu_select").change(function(){Cpu_get();});
+$("#memory_select").change(function(){Memory_get();});
+$("#network_select").change(function(){Network_get();});
