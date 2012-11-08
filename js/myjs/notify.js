3bd504c1ee638d4e3546fc97eabbe75b670dc179
myoan <motoki.yoan@gmail.com>
2012-11-08 23:37:56 +0900

    fix approve notify

--- js/myjs/notify.js
+++ js/myjs/notify.js
@@ -34,22 +34,28 @@ function dscript_notify_info(str) {
 
 var global_notice;
 
-function form_template(sentence, to) {
-   var $notify_body = $('#form_notice');
-   var html = '\
-   <form class="pf-form pform_custom" action method="post">\
-    <img src="./img/dscript_logo.png" width="80px" style="float: left; margin-left: -50px;"></img>\
-    <div>\
-      <div class="pf-element pf-heading">\
-        <h5>' + sentence + '</h5>\
-      </div>\
-      <div class="pf-element pf-buttons pf-centered" style="position: relative; margin: 0 auto 0 auto;">\
-        <button class="pf-button btn" type="button" onclick="notifier_allow_script(' + to + ');">Yes</button>\
-        <button class="pf-button btn btn-primary" type="button" onclick="notifier_deny_script(' + to + ');">No</button>\
-      </div>\
-    </div>\
-   </form>';
-   $notify_body.html(html);
+function form_template(body, to) {
+  var body_array = body.split(/\(|\)/);
+  var sentence = body_array[0];
+  var $notify_body = $('#form_notice');
+  var recv_words = body_array[1].replace(',', '').split(' ');
+  var allow_word = recv_words[1];
+  var allow_word = recv_words[3];
+  console.log(sentence + '(' + to + '): [' + allow_word + ', ' + deny_word + ']');
+  var html = '\
+  <form class="pf-form pform_custom" action method="post">\
+   <img src="./img/dscript_logo.png" width="80px" style="float: left; margin-left: -50px;"></img>\
+   <div>\
+     <div class="pf-element pf-heading">\
+       <h5>' + sentence + '</h5>\
+     </div>\
+     <div class="pf-element pf-buttons pf-centered" style="position: relative; margin: 0 auto 0 auto;">\
+       <button class="pf-button btn" type="button" onclick="notifier_allow_script(' + to + ');">' + allow_word + '</button>\
+       <button class="pf-button btn btn-primary" type="button" onclick="notifier_deny_script(' + to + ');">' + deny_word + '</button>\
+     </div>\
+   </div>\
+  </form>';
+  $notify_body.html(html);
   return $notify_body;
 }
 
